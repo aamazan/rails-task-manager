b8bdd3dea805ac46e827f617a813f17d6e02fa6e
Armaan Amazan <armaan.amazan@gmail.com>
2017-10-10 16:29:23 +0900

    I still can't believe I managed to do it

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,9 @@
 Rails.application.routes.draw do
-  get 'tasks/show'
-
-  get 'tasks/new'
-
-  get 'tasks/edit'
-
-  get 'tasks/index'
-
   root 'tasks#index'
-  resources :tasks
-  patch 'tasks/:id/complete', to: 'tasks#complete', as: :complete
-
+  resources :tasks do
+    member do
+       patch 'tasks/:id/complete', to: 'tasks#complete', as: :complete
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Template:
+# Task.create(name:"", description:"")
+Task.create(name:"thing 1", description:"identifying that thing")
+Task.create(name:"thing 2", description:"setting your sights on that thing")
+Task.create(name:"thing 3", description:"reaching for that thing")
+Task.create(name:"thing 4", description:"getting that thing")
+Task.create(name:"thing 5", description:"acknowledgement for getting that thing")
